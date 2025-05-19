e9102d57e1e75000a79c4b527f9d216597672668
fuatsezer <fuatsezer199696@gmail.com>
2025-05-19 18:18:31 +0300

    Deneme 7

--- test_list_comp_alistirmalar.py
+++ test_list_comp_alistirmalar.py
@@ -1,3 +1,6 @@
 """Contains test for list_comp_alistirmalar."""
+
 def test_imports():
+    """Test if list_comp_alistirmalar module imports successfully."""
     import list_comp_alistirmalar
+    assert list_comp_alistirmalar
--- test_python_alistirmalar.py
+++ test_python_alistirmalar.py
@@ -1,3 +1,5 @@
 """This module contains test for python_alistirmalar."""
 def test_imports():
+    """Test if python_alistirmalar module imports successfully."""
     import python_alistirmalar
+    assert python_alistirmalar
